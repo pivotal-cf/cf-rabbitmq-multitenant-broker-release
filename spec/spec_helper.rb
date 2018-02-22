d2489fa2f79688e9b2a9fb0b2fb511b2fc1fadef
Mark Hender <mhender@pivotal.io>
2018-02-22 08:51:25 +0000

    Refactor system tests
    * Refactor tests to remove prof and hula
    * We now use bosh2 cli commands
    * Use `cf curl` when it makes sense to
    * Improve performance
      - use methods when it makes sense to instead of lets
      - group tests that could potentially run in parallel because they have the same
    setup.
      - have two worlds, initial bosh deploy tests and a redeployed bosh
      world tests
    * Remove a test that was testing cloud foundry only
    
    [#154392090]

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,3 @@
-require 'prof/external_spec/spec_helper'
-require 'prof/environment/cloud_foundry'
-require 'prof/environment_manager'
 require 'yaml'
 require 'pry'
 require 'rspec/retry'
@@ -9,37 +6,11 @@ Dir[File.expand_path('support/**/*.rb', __dir__)].each do |file|
   require file
 end
 
-def environment
-  @environment ||= begin
-                     options = {
-                       bosh_manifest_path: ENV.fetch('BOSH_MANIFEST') { File.expand_path('../../manifests/cf-rabbitmq-broker.yml', __FILE__) },
-                       bosh_service_broker_job_name: 'cf-rabbitmq-multitenant-broker'
-                     }
-
-                     options[:cloud_foundry_domain]   = ENV.fetch('CF_DOMAIN', 'bosh-lite.com')
-                     options[:cloud_foundry_username] = ENV.fetch('CF_USERNAME', 'admin')
-                     options[:cloud_foundry_password] = ENV.fetch('CF_PASSWORD', 'admin')
-                     options[:cloud_foundry_api_url]  = ENV.fetch('CF_API', 'api.bosh-lite.com')
-
-                     options[:bosh_target]          = ENV.fetch('BOSH_TARGET', 'https://192.168.50.6:25555')
-                     options[:bosh_username]        = ENV['BOSH_USERNAME']
-                     options[:bosh_password]        = ENV['BOSH_PASSWORD']
-                     options[:ssh_gateway_host]     = URI.parse(options[:bosh_target]).host
-
-                     options[:ssh_gateway_username] = ENV.fetch('BOSH_SSH_USERNAME', 'vcap') if ENV.key?('BOSH_TARGET')
-
-                     options.keep_if do |key, value|
-                       not value.nil?
-                     end
-
-                     Prof::Environment::CloudFoundry.new(options)
-                   end
-end
-
-BOSH_CLI = ENV.fetch("BOSH_CLI", 'bosh')
+BOSH_CLI = ENV.fetch('BOSH_CLI', 'bosh')
 
 class Bosh2
-  def initialize(bosh_cli = 'bosh', deployment = 'cf-rabbitmq-multitenant-broker')
+  def initialize(bosh_cli = 'bosh')
+    deployment = ENV['DEPLOYMENT_NAME'] || 'cf-rabbitmq-multitenant-broker'
     @bosh_cli = "#{bosh_cli} -n -d #{deployment}"
 
     version = `#{@bosh_cli} --version`
@@ -61,8 +32,8 @@ class Bosh2
     Tempfile.open('manifest.yml') do |manifest_file|
       manifest_file.write(manifest.to_yaml)
       manifest_file.flush
-      output = ""
-      exit_code = ::Open3.popen3("#{@bosh_cli} deploy #{manifest_file.path}") do |stdin, stdout, stderr, wait_thr|
+      output = ''
+      exit_code = ::Open3.popen3("#{@bosh_cli} deploy #{manifest_file.path}") do |_stdin, stdout, _stderr, wait_thr|
         output << stdout.read
         wait_thr.value
       end
@@ -84,7 +55,7 @@ class Bosh2
 
   def manifest
     manifest = `#{@bosh_cli} manifest`
-    YAML.load(manifest)
+    YAML.safe_load(manifest)
   end
 
   def start(instance)
@@ -100,36 +71,157 @@ def bosh
   @bosh ||= Bosh2.new(BOSH_CLI)
 end
 
-def test_manifest
-  YAML.load_file(ENV.fetch('BOSH_MANIFEST'))
+class CloudFoundry
+  attr_reader :domain, :api_url
+
+  def initialize(domain, username, password, api_url)
+    @domain = domain
+
+    raise 'CF CLI is required' unless version.start_with?('cf version')
+
+    target(api_url)
+    login(username, password)
+  end
+
+  def target(api_url)
+    cf("api #{api_url} --skip-ssl-validation")
+  end
+
+  def login(username, password)
+    cf("auth #{username} #{password}")
+  end
+
+  def version
+    cf('--version')
+  end
+
+  def create_service_instance(service, plan, service_instance_name)
+    cf("create-service #{service} #{plan} #{service_instance_name}")
+  end
+
+  def bind_app_to_service(app_name, service_instance_name)
+    cf("bind-service #{app_name} #{service_instance_name}")
+  end
+
+  def unbind_app_from_service(app_name, service_instance_name)
+    cf("unbind-service #{app_name} #{service_instance_name}")
+  end
+
+  def service_key(service_instance_name, service_key_name)
+    guid = service_instance_guid(service_instance_name)
+    data = {
+      'service_instance_guid' => guid,
+      'name' => service_key_name
+    }
+    response = cf("curl -X POST /v2/service_keys -d '#{data.to_json}'")
+    key = JSON.parse(response)
+    yield key['entity']['credentials']
+    cf("curl -X DELETE /v2/service_keys/#{key['metadata']['guid']}")
+  end
+
+  def service_instance_guid(service_instance_name)
+    service_instance = cf("curl /v2/service_instances?q=name:#{service_instance_name}")
+    JSON.parse(service_instance)['resources'].first['metadata']['guid']
+  end
+
+  def push_app(app_path, name)
+    cf("push #{name} -p #{app_path} -n #{name} -d #{domain}")
+
+    App.new(name, "https://#{name}.#{domain}")
+  end
+
+  def start_app(name)
+    cf("start #{name}")
+  end
+
+  def restage_app(name)
+    cf("restage #{name}")
+  end
+
+  def url_for_app(app_name)
+    "https://#{app_name}.#{domain}"
+  end
+
+  def app_vcap_services(app_name)
+    all_apps = JSON.parse(cf('curl /v2/apps'))
+    testapp = all_apps['resources'].find do |app|
+      app['entity']['name'] == app_name
+    end
+
+    app_env = JSON.parse(cf("curl #{testapp['metadata']['url']}/env"))
+    app_env['system_env_json']['VCAP_SERVICES']
+  end
+
+  def create_org_and_space(org_name, space_name)
+    cf("create-org #{org_name}")
+    cf("target -o #{org_name}")
+    cf("create-space #{space_name}")
+    cf("target -s #{space_name}")
+    @org_name = org_name
+  end
+
+  def tear_down_org_and_space
+    cf("delete-org #{org_name} -f")
+  end
+
+  private
+
+  attr_reader :org_name
+
+  def cf(command)
+    p "cf #{command}" unless command.include?('auth')
+    stdout, stderr, status = Open3.capture3("cf #{command}")
+    raise "error executing command\n#{stderr}" if status.exitstatus == 1
+
+    stdout
+  end
 end
 
-def environment_manager
-  cf_environment = OpenStruct.new(:cloud_foundry => cf)
-  Prof::EnvironmentManager.new(cf_environment)
+class App < Struct.new(:name, :url)
 end
 
 def cf
-  @cf ||= environment.cloud_foundry
+  return @cf unless @cf.nil?
+
+  domain = ENV.fetch('CF_DOMAIN', 'bosh-lite.com')
+  username = ENV.fetch('CF_USERNAME', 'admin')
+  password = ENV.fetch('CF_PASSWORD', 'admin')
+  api_url = ENV.fetch('CF_API', 'api.bosh-lite.com')
+
+  @cf = CloudFoundry.new(domain, username, password, api_url)
+  @cf.create_org_and_space("cf-org-#{random_string}", "cf-space-#{random_string}")
+  @cf
+end
+
+def random_string
+  [*('A'..'Z')].sample(8).join
+end
+
+def test_manifest
+  YAML.load_file(ENV.fetch('BOSH_MANIFEST'))
 end
 
 def test_app
   @test_app ||= Prof::TestApp.new(path: File.expand_path('../../assets/rabbit-labrat', __FILE__))
 end
 
+def test_app_path
+  File.expand_path('../../assets/rabbit-labrat', __FILE__)
+end
+
 module ExcludeHelper
   def self.manifest
-    @bosh_manifest ||= YAML.load(File.read(ENV['BOSH_MANIFEST']))
+    @bosh_manifest ||= YAML.safe_load(File.read(ENV['BOSH_MANIFEST']))
   end
 
   def self.metrics_available?
     return unless ENV['BOSH_MANIFEST']
-    0 != manifest.fetch('releases').select{|i| i["name"] == "service-metrics" }.length
+    !manifest.fetch('releases').select { |i| i['name'] == 'service-metrics' }.empty?
   end
 
   def self.warnings
     message = "\n"
-    if !metrics_available?
+    unless metrics_available?
       message += "WARNING: Skipping metrics tests, metrics are not available in this manifest\n"
     end
 
@@ -137,30 +229,19 @@ module ExcludeHelper
   end
 end
 
-puts ExcludeHelper::warnings
+puts ExcludeHelper.warnings
 
 RSpec.configure do |config|
   config.include Matchers
   config.include TemplateHelpers, template: true
 
-  Matchers::prints_logs_on_failure = true
+  Matchers.prints_logs_on_failure = true
 
   config.filter_run :focus
   config.run_all_when_everything_filtered = true
-  config.filter_run_excluding :metrics => !ExcludeHelper::metrics_available?
-  config.filter_run_excluding :test_with_errands => ENV.has_key?('SKIP_ERRANDS')
-  config.filter_run_excluding :run_compliance_tests => (!ENV.has_key?('RUN_COMPLIANCE') && /darwin|mac os/ === RbConfig::CONFIG['host_os'] )
-
-  config.around do |example|
-    if example.metadata[:pushes_cf_app] || example.metadata[:creates_service_key]
-
-      environment_manager.isolate_cloud_foundry do
-        example.run
-      end
-    else
-      example.run
-    end
-  end
+  config.filter_run_excluding metrics: !ExcludeHelper.metrics_available?
+  config.filter_run_excluding test_with_errands: ENV.key?('SKIP_ERRANDS')
+  config.filter_run_excluding run_compliance_tests: (!ENV.key?('RUN_COMPLIANCE') && RbConfig::CONFIG['host_os'] === /darwin|mac os/)
 
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
--- spec/system/service_broker_spec.rb
+++ spec/system/service_broker_spec.rb
@@ -12,224 +12,203 @@ require 'prof/cloud_foundry'
 require 'prof/test_app'
 require 'rabbitmq/http/client'
 
-require "mqtt"
-require "stomp"
+require 'mqtt'
+require 'stomp'
 require 'net/https'
 require 'httparty'
 
 require File.expand_path('../../../assets/rabbit-labrat/lib/lab_rat/aggregate_health_checker.rb', __FILE__)
 
 RSpec.describe 'Using a Cloud Foundry service broker' do
-  let(:service_name) { 'p-rabbitmq' }
-
-  let(:service) do
-    Prof::MarketplaceService.new(
-      name: service_name,
-      plan: 'standard'
-    )
+  def service_offering
+    'standard'
   end
 
-  let(:rmq_server_admin_broker_username) do
-    rabbitmq_server_instance_group = manifest['instance_groups'].select{ |instance_group| instance_group['name'] == 'rmq' }.first
-    rabbitmq_server_job =  rabbitmq_server_instance_group['jobs'].select{ |job| job['name'] == 'rabbitmq-server'}.first
-    rabbitmq_server_job['properties']['administrators']['broker']['username']
+  def service_name
+    'p-rabbitmq-aleal'
   end
 
-  let(:rmq_server_admin_broker_password) do
-    rabbitmq_server_instance_group = manifest['instance_groups'].select{ |instance_group| instance_group['name'] == 'rmq' }.first
-    rabbitmq_server_job =  rabbitmq_server_instance_group['jobs'].select{ |job| job['name'] == 'rabbitmq-server'}.first
-    rabbitmq_server_job['properties']['administrators']['broker']['password']
+  def app_name
+    'testapp'
   end
 
-  let(:rmq_broker_username) do
-    rabbitmq_broker_registrar_instance_group = test_manifest['instance_groups'].select{ |instance_group| instance_group['name'] == 'broker-registrar' }.first
-    rabbitmq_broker_registrar_job =  rabbitmq_broker_registrar_instance_group['jobs'].select{ |job| job['name'] == 'broker-registrar'}.first
-    rabbitmq_broker_registrar_properties = rabbitmq_broker_registrar_job['properties']['broker']
-    rabbitmq_broker_registrar_properties[ 'username' ]
+  def service_instance_name
+    'testservicename'
   end
 
-  let(:rmq_broker_password) do
-    rabbitmq_broker_registrar_instance_group = test_manifest['instance_groups'].select{ |instance_group| instance_group['name'] == 'broker-registrar' }.first
-    rabbitmq_broker_registrar_job =  rabbitmq_broker_registrar_instance_group['jobs'].select{ |job| job['name'] == 'broker-registrar'}.first
-    rabbitmq_broker_registrar_properties = rabbitmq_broker_registrar_job['properties']['broker']
-    rabbitmq_broker_registrar_properties[ 'password' ]
-  end
+  context 'when bound to a service instance' do
+    before :all do
+      @app = cf.push_app(test_app_path, app_name)
 
-  let(:rmq_broker_host) do
-    rabbitmq_broker_registrar_instance_group = test_manifest['instance_groups'].select{ |instance_group| instance_group['name'] == 'broker-registrar' }.first
-    rabbitmq_broker_registrar_job =  rabbitmq_broker_registrar_instance_group['jobs'].select{ |job| job['name'] == 'broker-registrar'}.first
-    rabbitmq_broker_registrar_properties = rabbitmq_broker_registrar_job['properties']['broker']
-    protocol = rabbitmq_broker_registrar_properties[ 'protocol' ]
-    host = rabbitmq_broker_registrar_properties[ 'host' ]
-    URI.parse("#{protocol}://#{host}")
-  end
-
-  let(:broker_catalog) do
-    catalog_uri = URI.join(rmq_broker_host, '/v2/catalog')
-    req = Net::HTTP::Get.new(catalog_uri)
-    req.basic_auth(rmq_broker_username, rmq_broker_password)
-    response = Net::HTTP.start(rmq_broker_host.hostname, rmq_broker_host.port, :use_ssl => rmq_broker_host.scheme == 'https', :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
-      http.request(req)
+      cf.create_service_instance(service_name, service_offering, service_instance_name)
+      cf.bind_app_to_service(app_name, service_instance_name)
+      cf.restage_app(app_name)
     end
-    JSON.parse(response.body)
-  end
 
-  context 'default deployment'  do
+    after :all do
+      cf.tear_down_org_and_space
+    end
+
     it 'provides default connectivity', :pushes_cf_app do
-      cf.push_app_and_bind_with_service(test_app, service) do |app, _|
-        provides_amqp_connectivity(app)
-        provides_mqtt_connectivity(app)
-        provides_stomp_connectivity(app)
-      end
+      provides_amqp_connectivity(@app)
+      provides_mqtt_connectivity(@app)
+      provides_stomp_connectivity(@app)
     end
 
-    it 'fails to connect if bindings are deleted', :pushes_cf_app do
-      cf.push_app_and_bind_with_service(test_app, service) do |app, service_instance|
-        cf.unbind_app_from_service(app, service_instance)
+    it 'should provision a service key with defaults' do
+      service_key_name = 'testservicekey'
 
-        provides_no_amqp_connectivity(app)
-        provides_no_mqtt_connectivity(app)
- 
-        # Check #150334805
-        # provides_no_stomp_connectivity(app)
-      end
-    end
-  end
-
-  context 'when stomp plugin is disabled'  do
-    before(:context) do
-      bosh.redeploy do |manifest|
-        rabbitmq_server_instance_group = manifest['instance_groups'].select{ |instance_group| instance_group['name'] == 'rmq' }.first
-        rabbitmq_server_job =  rabbitmq_server_instance_group['jobs'].select{ |job| job['name'] == 'rabbitmq-server'}.first
-        service_properties = rabbitmq_server_job['properties']['rabbitmq-server']['plugins'] = ['rabbitmq_management','rabbitmq_mqtt']
-      end
-    end
-
-    after(:context) do
-      bosh.deploy(test_manifest)
-    end
-
-    it 'provides only amqp and mqtt connectivity', :pushes_cf_app do
-      cf.push_app_and_bind_with_service(test_app, service) do |app, _|
-        provides_amqp_connectivity(app)
-        provides_mqtt_connectivity(app)
-        provides_no_stomp_connectivity(app)
-      end
-    end
-  end
-
-  context 'when broker is configured with HA policy' do
-    before(:context) do
-      bosh.redeploy do |manifest|
-        rabbitmq_broker_instance_group = manifest['instance_groups'].select{ |instance_group| instance_group['name'] == 'rmq-broker' }.first
-        rabbitmq_broker_job =  rabbitmq_broker_instance_group['jobs'].select{ |job| job['name'] == 'rabbitmq-broker'}.first
-        service_properties = rabbitmq_broker_job['properties']['rabbitmq-broker']['rabbitmq']['operator_set_policy'] = {
-          'enabled' => true,
-          'policy_name' => "operator_set_policy",
-          'policy_definition' => "{\"ha-mode\":\"exactly\",\"ha-params\":2,\"ha-sync-mode\":\"automatic\"}",
-          'policy_priority' => 50
-        }
-      end
-    end
-
-    after(:context) do
-      bosh.deploy(test_manifest)
-    end
-
-    it 'sets queue policy to each created service instance', :pushes_cf_app do
-      cf.push_app_and_bind_with_service(test_app, service) do |app, _|
-        provides_mirrored_queue_policy_as_a_default(app)
-      end
-    end
-  end
-
-  context 'when provisioning a service key' do
-    it 'provides defaults', :creates_service_key do
-      cf.provision_and_create_service_key(service) do |service_instance, service_key, service_key_data|
+      cf.service_key(service_instance_name, service_key_name) do |service_key_data|
         check_that_amqp_connection_data_is_present_in(service_key_data)
         check_that_stomp_connection_data_is_present_in(service_key_data)
       end
     end
   end
 
-  context 'when deprovisioning a service key' do
-    it 'is no longer listed in service-keys', :creates_service_key do
-      cf.provision_and_create_service_key(service) do |service_instance, service_key, service_key_data|
-        @service_instance = service_instance
-        @service_key = service_key
-        @service_key_data = service_key_data
+  context 'when unbinding a service instance' do
+    after :each do
+      cf.tear_down_org_and_space
+    end
 
-        cf.delete_service_key(@service_instance, @service_key)
+    it 'fails to connect if bindings are deleted', :pushes_cf_app do
+      app = cf.push_app(test_app_path, app_name)
+      cf.create_service_instance(service_name, service_offering, service_instance_name)
+      cf.bind_app_to_service(app_name, service_instance_name)
+      cf.restage_app(app_name)
 
-        expect(cf.list_service_keys(@service_instance)).to_not include(@service_key)
-      end
+      cf.unbind_app_from_service(app_name, service_instance_name)
+
+      provides_no_amqp_connectivity(app)
+      provides_no_mqtt_connectivity(app)
     end
   end
 
-  context 'when broker is configured' do
-    context 'when the service broker is configured with particular service metadata' do
+  describe 'redeploying cf-rabbitmq-multitenant-broker' do
+    before :all do
+      bosh.redeploy do |manifest|
+        # Remove STOMP plugin
+        rabbitmq_server_instance_group = manifest['instance_groups'].select { |instance_group| instance_group['name'] == 'rmq' }.first
+        rabbitmq_server_job = rabbitmq_server_instance_group['jobs'].select { |job| job['name'] == 'rabbitmq-server' }.first
+        rabbitmq_server_job['properties']['rabbitmq-server']['plugins'] = %w[rabbitmq_management rabbitmq_mqtt]
+
+        # Broker with HA policy
+        rabbitmq_broker_instance_group = manifest['instance_groups'].select { |instance_group| instance_group['name'] == 'rmq-broker' }.first
+        rabbitmq_broker_job = rabbitmq_broker_instance_group['jobs'].select { |job| job['name'] == 'rabbitmq-broker' }.first
+        rabbitmq_broker_job['properties']['rabbitmq-broker']['rabbitmq']['operator_set_policy'] = {
+          'enabled' => true,
+          'policy_name' => 'operator_set_policy',
+          'policy_definition' => '{"ha-mode":"exactly","ha-params":2,"ha-sync-mode":"automatic"}',
+          'policy_priority' => 50
+        }
+
+        # Configuring broker service metadata
+        rabbitmq_broker_service = rabbitmq_broker_job['properties']['rabbitmq-broker']['service']
+        rabbitmq_broker_service['name'] = 'service-name'
+        rabbitmq_broker_service['display_name'] = 'apps-manager-test-name'
+        rabbitmq_broker_service['offering_description'] = 'Some description of our service'
+        rabbitmq_broker_service['long_description'] = 'Some long description of our service'
+        rabbitmq_broker_service['icon_image'] = 'image-uri'
+        rabbitmq_broker_service['provider_display_name'] = 'CompanyName'
+        rabbitmq_broker_service['documentation_url'] = 'https://documentation.url'
+        rabbitmq_broker_service['support_url'] = 'https://support.url'
+      end
+
+      # Setup Cloud Foundry
+      @app = cf.push_app(test_app_path, app_name)
+      cf.create_service_instance(service_name, service_offering, service_instance_name)
+      cf.bind_app_to_service(app_name, service_instance_name)
+      cf.restage_app(app_name)
+    end
+
+    after :all do
+      bosh.deploy(test_manifest)
+      cf.tear_down_org_and_space
+    end
+
+    context 'when stomp plugin is disabled' do
+      it 'provides only amqp and mqtt connectivity', :pushes_cf_app do
+        provides_amqp_connectivity(@app)
+        provides_mqtt_connectivity(@app)
+        provides_no_stomp_connectivity(@app)
+      end
+    end
+
+    context 'when broker is configured with HA policy' do
+      it 'sets queue policy to each created service instance', :pushes_cf_app do
+        provides_mirrored_queue_policy_as_a_default(@app, service_name)
+      end
+    end
+
+    describe 'the catalog' do
+      let(:rmq_broker_username) do
+        rabbitmq_broker_registrar_instance_group = test_manifest['instance_groups'].select { |instance_group| instance_group['name'] == 'broker-registrar' }.first
+        rabbitmq_broker_registrar_job = rabbitmq_broker_registrar_instance_group['jobs'].select { |job| job['name'] == 'broker-registrar' }.first
+        rabbitmq_broker_registrar_properties = rabbitmq_broker_registrar_job['properties']['broker']
+        rabbitmq_broker_registrar_properties['username']
+      end
+      let(:rmq_broker_password) do
+        rabbitmq_broker_registrar_instance_group = test_manifest['instance_groups'].select { |instance_group| instance_group['name'] == 'broker-registrar' }.first
+        rabbitmq_broker_registrar_job = rabbitmq_broker_registrar_instance_group['jobs'].select { |job| job['name'] == 'broker-registrar' }.first
+        rabbitmq_broker_registrar_properties = rabbitmq_broker_registrar_job['properties']['broker']
+        rabbitmq_broker_registrar_properties['password']
+      end
+      let(:rmq_broker_host) do
+        rabbitmq_broker_registrar_instance_group = test_manifest['instance_groups'].select { |instance_group| instance_group['name'] == 'broker-registrar' }.first
+        rabbitmq_broker_registrar_job = rabbitmq_broker_registrar_instance_group['jobs'].select { |job| job['name'] == 'broker-registrar' }.first
+        rabbitmq_broker_registrar_properties = rabbitmq_broker_registrar_job['properties']['broker']
+        protocol = rabbitmq_broker_registrar_properties['protocol']
+        host = rabbitmq_broker_registrar_properties['host']
+        URI.parse("#{protocol}://#{host}")
+      end
+      let(:broker_catalog) do
+        catalog_uri = URI.join(rmq_broker_host, '/v2/catalog')
+        req = Net::HTTP::Get.new(catalog_uri)
+        req.basic_auth(rmq_broker_username, rmq_broker_password)
+        response = Net::HTTP.start(rmq_broker_host.hostname, rmq_broker_host.port, use_ssl: rmq_broker_host.scheme == 'https', verify_mode: OpenSSL::SSL::VERIFY_NONE) do |http|
+          http.request(req)
+        end
+        JSON.parse(response.body)
+      end
       let(:service_info) { broker_catalog['services'].first }
       let(:broker_catalog_metadata) { service_info['metadata'] }
 
-      before(:all) do
-        bosh.redeploy do |manifest|
-          rabbitmq_broker_instance_group = manifest['instance_groups'].select{ |instance_group| instance_group['name'] == 'rmq-broker' }.first
-          rabbitmq_broker_job =  rabbitmq_broker_instance_group['jobs'].select{ |job| job['name'] == 'rabbitmq-broker'}.first
-          service_properties = rabbitmq_broker_job['properties']['rabbitmq-broker']['service']
-          service_properties['name'] = "service-name"
-          service_properties['display_name'] = "apps-manager-test-name"
-          service_properties['offering_description'] = "Some description of our service"
-          service_properties['long_description'] = "Some long description of our service"
-          service_properties['icon_image'] = "image-uri"
-          service_properties['provider_display_name'] = "CompanyName"
-          service_properties['documentation_url'] = "https://documentation.url"
-          service_properties['support_url'] = "https://support.url"
-        end
+      it 'has the correct name' do
+        expect(service_info['name']).to eq('service-name')
       end
 
-      after(:all) do
-        bosh.deploy(test_manifest)
+      it 'has the correct description' do
+        expect(service_info['description']).to eq('Some description of our service')
       end
 
-      describe 'the catalog' do
-        it 'has the correct name' do
-          expect(service_info['name']).to eq("service-name")
-        end
+      it 'has the correct display name' do
+        expect(broker_catalog_metadata['displayName']).to eq('apps-manager-test-name')
+      end
 
-        it 'has the correct description' do
-          expect(service_info['description']).to eq("Some description of our service")
-        end
+      it 'has the correct long description' do
+        expect(broker_catalog_metadata['longDescription']).to eq('Some long description of our service')
+      end
 
-        it 'has the correct display name' do
-          expect(broker_catalog_metadata['displayName']).to eq("apps-manager-test-name")
-        end
+      it 'has the correct image icon' do
+        expect(broker_catalog_metadata['imageUrl']).to eq('data:image/png;base64,image-uri')
+      end
 
-        it 'has the correct long description' do
-          expect(broker_catalog_metadata['longDescription']).to eq("Some long description of our service")
-        end
+      it 'has the correct provider display name' do
+        expect(broker_catalog_metadata['providerDisplayName']).to eq('CompanyName')
+      end
 
-        it 'has the correct image icon' do
-          expect(broker_catalog_metadata['imageUrl']).to eq("data:image/png;base64,image-uri")
-        end
+      it 'has the correct documentation url' do
+        expect(broker_catalog_metadata['documentationUrl']).to eq('https://documentation.url')
+      end
 
-        it 'has the correct provider display name' do
-          expect(broker_catalog_metadata['providerDisplayName']).to eq("CompanyName")
-        end
-
-        it 'has the correct documentation url' do
-          expect(broker_catalog_metadata['documentationUrl']).to eq("https://documentation.url")
-        end
-
-        it 'has the correct support url' do
-          expect(broker_catalog_metadata['supportUrl']).to eq("https://support.url")
-        end
+      it 'has the correct support url' do
+        expect(broker_catalog_metadata['supportUrl']).to eq('https://support.url')
       end
     end
   end
 end
 
 def get(url)
-  HTTParty.get(url, {verify: false, timeout: 2})
+  HTTParty.get(url, verify: false, timeout: 2)
 end
 
 def provides_amqp_connectivity(app)
@@ -267,12 +246,11 @@ end
 
 def provides_no_connectivity_for(app, protocol)
   # This is a work-around for #144893311
-  begin
-    response = get("#{app.url}/services/rabbitmq/protocols/#{protocol}")
-    expect(response.code).to eql(500)
-  rescue Net::ReadTimeout => e
-    puts "Caught exception #{e}!"
-  end
+
+  response = get("#{app.url}/services/rabbitmq/protocols/#{protocol}")
+  expect(response.code).to eql(500)
+rescue Net::ReadTimeout => e
+  puts "Caught exception #{e}!"
 end
 
 def check_that_amqp_connection_data_is_present_in(service_key_data)
@@ -280,7 +258,7 @@ def check_that_amqp_connection_data_is_present_in(service_key_data)
 end
 
 def check_that_stomp_connection_data_is_present_in(service_key_data)
-  check_connection_data(service_key_data, 'stomp', 61613)
+  check_connection_data(service_key_data, 'stomp', 61_613)
 end
 
 def check_connection_data(service_key_data, protocol, port)
@@ -300,8 +278,9 @@ def check_connection_data(service_key_data, protocol, port)
   expect(service_key_data['protocols'][protocol]['vhost']).not_to be_empty
 end
 
-def provides_mirrored_queue_policy_as_a_default(app)
+def provides_mirrored_queue_policy_as_a_default(app, service_name)
   vcap_services = cf.app_vcap_services(app.name)
+
   credentials = vcap_services[service_name].first['credentials']
   http_api_uris = credentials['http_api_uris']
   vhost = credentials['vhost']
