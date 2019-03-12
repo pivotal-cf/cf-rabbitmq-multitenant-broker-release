68a55fd0eb03522b49c4fee9182805da986ecec9
George Blue <gblue@pivotal.io>
2019-03-12 17:13:09 +0000

    [rabbitmq-service-broker] tests for multiple SIs and bindings
    [#164338662]

--- src/rabbitmq-service-broker/integrationtests/lifecycle_test.go
+++ src/rabbitmq-service-broker/integrationtests/lifecycle_test.go
@@ -18,30 +18,11 @@ var _ = Describe("the lifecycle of a service instance", func() {
 		bindingID = "22222222-2222-2222-2222-222222222222"
 	)
 
-	var provisionDetails, bindDetails []byte
-
-	BeforeEach(func() {
-		var err error
-		provisionDetails, err = json.Marshal(map[string]string{
-			"service_id":        serviceID,
-			"plan_id":           planID,
-			"organization_guid": "fake-org-guid",
-			"space_guid":        "fake-space-guid",
-		})
-		Expect(err).NotTo(HaveOccurred())
-
-		bindDetails, err = json.Marshal(map[string]string{
-			"service_id": serviceID,
-			"plan_id":    planID,
-		})
-		Expect(err).NotTo(HaveOccurred())
-	})
-
-	It("succeeds", func() {
+	It("succeeds for one SI and binding", func() {
 		serviceInstanceID := "7ef8c450-6aa1-4d8a-a56f-a8bb4ddd1de7"
 
 		By("sending a provision request")
-		provisionResponse, provisionBody := doRequest(http.MethodPut, provisionURL(serviceInstanceID), bytes.NewReader(provisionDetails))
+		provisionResponse, provisionBody := provision(serviceInstanceID, serviceID, planID)
 		Expect(provisionResponse.StatusCode).To(Equal(http.StatusCreated), string(provisionBody))
 
 		By("checking that a dashboard URL is returned")
@@ -75,7 +56,7 @@ var _ = Describe("the lifecycle of a service instance", func() {
 		Expect(policies[0].Priority).To(Equal(50))
 
 		By("sending a binding request")
-		bindResponse, bindBody := doRequest(http.MethodPut, bindURL(serviceInstanceID, bindingID), bytes.NewReader(bindDetails))
+		bindResponse, bindBody := bind(bindingID, serviceInstanceID, serviceID, planID)
 		Expect(bindResponse.StatusCode).To(Equal(http.StatusCreated), string(bindBody))
 
 		By("checking the binding credentials")
@@ -102,17 +83,69 @@ var _ = Describe("the lifecycle of a service instance", func() {
 		Expect(rabbitErr.StatusCode).To(Equal(http.StatusNotFound))
 	})
 
+	It("succeeds for many SIs and bindings", func() {
+		serviceInstanceIDa := "7ef8c450-6aa1-4d8a-a56f-a8bb4ddd1ae7"
+		serviceInstanceIDb := "7ef8c450-6aa1-4d8a-a56f-a8bb4ddd1be7"
+		serviceInstanceIDc := "7ef8c450-6aa1-4d8a-a56f-a8bb4ddd1ce7"
+		bindingIDa := "7ef8c450-8b1c-4d8a-a56f-a8bb4ddd1ae7"
+		bindingIDb := "7ef8c450-8b1c-4d8a-a56f-a8bb4ddd1be7"
+		bindingIDc := "7ef8c450-8b1c-4d8a-a56f-a8bb4ddd1ce7"
+		bindingIDd := "7ef8c450-8b1c-4d8a-a56f-a8bb4ddd1de7"
+		bindingIDe := "7ef8c450-8b1c-4d8a-a56f-a8bb4ddd1ee7"
+		bindingIDf := "7ef8c450-8b1c-4d8a-a56f-a8bb4ddd1fe7"
+
+		By("sending a provision requests")
+		provisionResponse, provisionBody := provision(serviceInstanceIDa, serviceID, planID)
+		Expect(provisionResponse.StatusCode).To(Equal(http.StatusCreated), string(provisionBody))
+		provisionResponse, provisionBody = provision(serviceInstanceIDb, serviceID, planID)
+		Expect(provisionResponse.StatusCode).To(Equal(http.StatusCreated), string(provisionBody))
+		provisionResponse, provisionBody = provision(serviceInstanceIDc, serviceID, planID)
+		Expect(provisionResponse.StatusCode).To(Equal(http.StatusCreated), string(provisionBody))
+
+		By("sending a binding requests")
+		bindResponse1, bindBody1 := bind(bindingIDa, serviceInstanceIDa, serviceID, planID)
+		Expect(bindResponse1.StatusCode).To(Equal(http.StatusCreated), string(bindBody1))
+		bindResponse2, bindBody2 := bind(bindingIDb, serviceInstanceIDa, serviceID, planID)
+		Expect(bindResponse2.StatusCode).To(Equal(http.StatusCreated), string(bindBody2))
+		bindResponse3, bindBody3 := bind(bindingIDc, serviceInstanceIDa, serviceID, planID)
+		Expect(bindResponse3.StatusCode).To(Equal(http.StatusCreated), string(bindBody3))
+		bindResponse4, bindBody4 := bind(bindingIDd, serviceInstanceIDb, serviceID, planID)
+		Expect(bindResponse4.StatusCode).To(Equal(http.StatusCreated), string(bindBody4))
+		bindResponse5, bindBody5 := bind(bindingIDe, serviceInstanceIDb, serviceID, planID)
+		Expect(bindResponse5.StatusCode).To(Equal(http.StatusCreated), string(bindBody5))
+		bindResponse6, bindBody6 := bind(bindingIDf, serviceInstanceIDb, serviceID, planID)
+		Expect(bindResponse6.StatusCode).To(Equal(http.StatusCreated), string(bindBody6))
+
+		By("checking the binding passwords are different")
+		var binding1, binding2 map[string]interface{}
+		Expect(json.Unmarshal(bindBody1, &binding1)).To(Succeed())
+		Expect(json.Unmarshal(bindBody2, &binding2)).To(Succeed())
+		Expect(binding1["credentials"].(map[string]interface{})["password"]).NotTo(Equal(binding2["credentials"].(map[string]interface{})["password"]))
+
+		By("checking that that binding user does not have access to another instance's vhost")
+		_, err := rmqClient.GetPermissionsIn(serviceInstanceIDa, bindingIDd)
+		Expect(err).To(HaveOccurred())
+
+		By("sending a deprovision request")
+		deprovisionResponse1, deprovisionBody1 := doRequest(http.MethodDelete, deprovisionURL(serviceInstanceIDa, serviceID, planID), nil)
+		Expect(deprovisionResponse1.StatusCode).To(Equal(http.StatusOK), string(deprovisionBody1))
+		deprovisionResponse2, deprovisionBody2 := doRequest(http.MethodDelete, deprovisionURL(serviceInstanceIDb, serviceID, planID), nil)
+		Expect(deprovisionResponse2.StatusCode).To(Equal(http.StatusOK), string(deprovisionBody2))
+		deprovisionResponse3, deprovisionBody3 := doRequest(http.MethodDelete, deprovisionURL(serviceInstanceIDc, serviceID, planID), nil)
+		Expect(deprovisionResponse3.StatusCode).To(Equal(http.StatusOK), string(deprovisionBody3))
+	})
+
 	Context("provisioning", func() {
 		When("a service instance with the same ID has already been provisioned", func() {
 			const serviceInstanceID = "9ef8c450-6aa1-6d8a-b56f-a8bb4ddd1de4"
 
 			BeforeEach(func() {
-				response, body := doRequest(http.MethodPut, provisionURL(serviceInstanceID), bytes.NewReader(provisionDetails))
+				response, body := provision(serviceInstanceID, serviceID, planID)
 				Expect(response.StatusCode).To(Equal(http.StatusCreated), string(body))
 			})
 
 			It("fails with HTTP 409", func() {
-				response, _ := doRequest(http.MethodPut, provisionURL(serviceInstanceID), bytes.NewReader(provisionDetails))
+				response, _ := provision(serviceInstanceID, serviceID, planID)
 				Expect(response.StatusCode).To(Equal(http.StatusConflict))
 			})
 		})
@@ -139,4 +172,26 @@ func deprovisionURL(serviceInstanceID, serviceID, planID string) string {
 
 func bindURL(serviceInstanceID, bindingID string) string {
 	return fmt.Sprintf("%sservice_instances/%s/service_bindings/%s", baseURL, serviceInstanceID, bindingID)
+}
+
+func provision(serviceInstanceID, serviceID, planID string) (*http.Response, []byte) {
+	provisionDetails, err := json.Marshal(map[string]string{
+		"service_id":        serviceID,
+		"plan_id":           planID,
+		"organization_guid": "fake-org-guid",
+		"space_guid":        "fake-space-guid",
+	})
+	Expect(err).NotTo(HaveOccurred())
+
+	return doRequest(http.MethodPut, provisionURL(serviceInstanceID), bytes.NewReader(provisionDetails))
+}
+
+func bind(bindingID, serviceInstanceID, serviceID, planID string) (*http.Response, []byte) {
+	bindDetails, err := json.Marshal(map[string]string{
+		"service_id": serviceID,
+		"plan_id":    planID,
+	})
+	Expect(err).NotTo(HaveOccurred())
+
+	return doRequest(http.MethodPut, bindURL(serviceInstanceID, bindingID), bytes.NewReader(bindDetails))
 }
