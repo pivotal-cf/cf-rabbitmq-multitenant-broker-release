bb4ebf15ee6bcacce4065d388e725cc345412782
Connor Rogers <crogers@pivotal.io>
2019-05-03 14:47:45 +0100

    Prefer to use client credentials when present
    Provided the tile provides admin client credentials, this client will be
    used in the test instead of an admin user. If a tile provides both
    client and user configuration, client config will be preferentially
    used.
    
    [#164472827]
    
    Co-authored-by: Alex Blease <ablease@pivotal.io>

--- spec/unit/templates/broker-deregistrar_errand_spec.rb
+++ spec/unit/templates/broker-deregistrar_errand_spec.rb
@@ -1,59 +1,88 @@
 RSpec.describe 'broker deregistration errand template', template: true do
-	let(:output) {
-		compiled_template('broker-deregistrar', 'errand.sh', @manifest_properties)
-	}
+  let(:output) {
+    compiled_template('broker-deregistrar', 'errand.sh', manifest_properties)
+  }
 
-	before(:each) do
-		@manifest_properties = {
-			'cf' => { 'skip_ssl_validation' => false },
-			'broker' => {
-				'name' => 'broker_name',
-				'service' => { 'name' => "service_name" }
-			}
-		}
-	end
+  describe 'authentication methods' do
+    context 'when user credentials are configured' do
+      let(:manifest_properties) do
+        {
+          'cf' => {
+            'admin_username' => 'some-username',
+            'admin_password' => 'some-password'
+          }
+        }
+      end
 
-	it 'skips ssl validation' do
-		expect(output).not_to include '--skip-ssl-validation'
-		expect(output).to include 'cf api'
-	end
+      it 'authenticates using admin username and password' do
+        expect(output).to include 'CF_AUTH_COMMAND="$CF_ADMIN_USERNAME $CF_ADMIN_PASSWORD"'
+      end
+    end
 
-	it 'purges the correct service offering' do
-		expect(output).to include "cf purge-service-offering -f 'service_name'"
-	end
+    context 'when oauth client is configured' do
+      let(:manifest_properties) do
+        {
+          'cf' => {
+            'admin_client' => 'some-client',
+            'admin_client_secret' => 'some-secret'
+          }
+        }
+      end
 
-	it 'deletes the correct broker' do
-		expect(output).to include "cf delete-service-broker -f 'broker_name'"
-	end
+      it 'authenticates with cf api using admin client and secret' do
+        expect(output).to include 'CF_AUTH_COMMAND="--client-credentials $CF_ADMIN_CLIENT $CF_ADMIN_CLIENT_SECRET"'
+      end
+    end
 
-	context 'when ssl validation is skipped' do
-		before(:each) do
-			@manifest_properties['cf']['skip_ssl_validation'] = true
-		end
+    context 'when both oauth client and admin user are configured' do
+      let(:manifest_properties) do
+        {
+          'cf' => {
+            'admin_username' => 'some-username',
+            'admin_password' => 'some-password',
+            'admin_client' => 'some-client',
+            'admin_client_secret' => 'some-secret'
+          }
+        }
+      end
 
-		it 'skips ssl validation' do
-			expect(output).to include 'cf api --skip-ssl-validation'
-		end
-	end
+      it 'authenticates with cf api using admin client and secret' do
+        expect(output).to include 'CF_AUTH_COMMAND="--client-credentials $CF_ADMIN_CLIENT $CF_ADMIN_CLIENT_SECRET"'
+      end
+    end
+  end
 
-	context 'when I set my broker service name' do
-		before(:each) do
-			@manifest_properties['broker']['service']['name'] = "broker_service_name"
-		end
+  context 'when ssl validation is skipped' do
+    let(:manifest_properties) { { 'cf' => { 'skip_ssl_validation' => true } } }
 
-		it 'purges the correct service offering' do
-			expect(output).to include "cf purge-service-offering -f 'broker_service_name'"
-		end
-	end
+    it 'skips ssl validation' do
+      expect(output).to include 'cf api --skip-ssl-validation'
+    end
+  end
 
-	context 'when I set my broker name' do
-		before(:each) do
-			@manifest_properties['broker']['name'] = "changed_broker_name"
-		end
+  context 'when ssl validation is respected' do
+    let(:manifest_properties) { { 'cf' => { 'skip_ssl_validation' => false } } }
 
-		it 'deletes the correct broker' do
-			expect(output).to include "cf delete-service-broker -f 'changed_broker_name'"
-		end
-	end
+    it 'do not skip ssl validation' do
+      expect(output).not_to include '--skip-ssl-validation'
+      expect(output).to include 'cf api'
+    end
+  end
+
+  context 'when I set my broker service name' do
+		let(:manifest_properties) { { 'broker' => { 'service' => { 'name' => 'broker_service_name' } } } }
+
+    it 'purges the correct service offering' do
+      expect(output).to include "cf purge-service-offering -f 'broker_service_name'"
+    end
+  end
+
+  context 'when deleting the service broker' do
+    let(:manifest_properties) { { 'broker' => { 'name' => 'changed_broker_name' } } }
+
+    it 'deletes the correct broker' do
+      expect(output).to include "cf delete-service-broker -f 'changed_broker_name'"
+    end
+  end
 end
 
--- spec/unit/templates/broker-registrar_errand_spec.rb
+++ spec/unit/templates/broker-registrar_errand_spec.rb
@@ -1,48 +1,96 @@
 RSpec.describe 'broker-registrar errand template', template: true do
-	let(:output) {
-		compiled_template('broker-registrar', 'errand.sh', manifest_properties)
-	}
+  let(:output) {
+    compiled_template('broker-registrar', 'errand.sh', manifest_properties)
+  }
 
-	context 'when the manifest says to perform ssl validation' do
-		let(:manifest_properties) do
-			{'cf' => { 'skip_ssl_validation' => false }}
-		end
+  describe 'authentication methods' do
+    context 'when user credentials are configured' do
+      let(:manifest_properties) do
+        {
+          'cf' => {
+            'admin_username' => 'some-username',
+            'admin_password' => 'some-password',
+          }
+        }
+      end
 
-		it 'performs ssl validation in errand.sh' do
-			expect(output).not_to include '--skip-ssl-validation'
-			expect(output).to include 'cf api'
-		end
-	end
+      it 'authenticates with cf api using admin username and password' do
+        expect(output).to include 'CF_AUTH_COMMAND="$CF_ADMIN_USERNAME $CF_ADMIN_PASSWORD"'
+      end
+    end
 
-	context 'when the manifest says to skip ssl validation' do
-		let(:manifest_properties) do
-			{'cf' => { 'skip_ssl_validation' => true }}
-		end
+    context 'when oauth client is configured' do
+      let(:manifest_properties) do
+        {
+          'cf' => {
+            'admin_client' => 'some-client',
+            'admin_client_secret' => 'some-secret'
+          }
+        }
+      end
 
-		it 'skips ssl validation in errand.sh' do
-			expect(output).to include 'cf api --skip-ssl-validation'
-		end
-	end
+      it 'authenticates with cf api using admin client and secret' do
+        expect(output).to include 'CF_AUTH_COMMAND="--client-credentials $CF_ADMIN_CLIENT $CF_ADMIN_CLIENT_SECRET"'
+      end
+    end
+
+    context 'when both oauth client and admin user are configured' do
+      let(:manifest_properties) do
+        {
+          'cf' => {
+            'admin_username' => 'some-username',
+            'admin_password' => 'some-password',
+            'admin_client' => 'some-client',
+            'admin_client_secret' => 'some-secret'
+          }
+        }
+      end
+
+      it 'authenticates with cf api using admin client and secret' do
+        expect(output).to include 'CF_AUTH_COMMAND="--client-credentials $CF_ADMIN_CLIENT $CF_ADMIN_CLIENT_SECRET"'
+      end
+    end
+  end
+
+  context 'when the manifest says to perform ssl validation' do
+    let(:manifest_properties) do
+      { 'cf' => { 'skip_ssl_validation' => false } }
+    end
+
+    it 'performs ssl validation in errand.sh' do
+      expect(output).not_to include '--skip-ssl-validation'
+      expect(output).to include 'cf api'
+    end
+  end
+
+  context 'when the manifest says to skip ssl validation' do
+    let(:manifest_properties) do
+      { 'cf' => { 'skip_ssl_validation' => true } }
+    end
+
+    it 'skips ssl validation in errand.sh' do
+      expect(output).to include 'cf api --skip-ssl-validation'
+    end
+  end
 
   context 'when the orgs propery is not set' do
-		let(:manifest_properties) do
-			{'broker-registrar' => { 'orgs' => [] }}
-		end
+    let(:manifest_properties) do
+      { 'broker-registrar' => { 'orgs' => [] } }
+    end
 
     it 'enables access to the service broker for all orgs' do
-			expect(output).to include 'cf enable-service-access "$SERVICE_NAME"'
-		end
-	end
+      expect(output).to include 'cf enable-service-access "$SERVICE_NAME"'
+    end
+  end
 
   context 'when the orgs property is set' do
-		let(:manifest_properties) do
-			{'broker-registrar' => { 'orgs' => ['org1','org2'] }}
-		end
+    let(:manifest_properties) do
+      { 'broker-registrar' => { 'orgs' => ['org1','org2'] }}
+    end
 
     it 'enables access to the service broker for the specified orgs only' do
-			expect(output).to include 'cf enable-service-access "$SERVICE_NAME" -o org1'
-			expect(output).to include 'cf enable-service-access "$SERVICE_NAME" -o org2'
-		end
-	end
+      expect(output).to include 'cf enable-service-access "$SERVICE_NAME" -o org1'
+      expect(output).to include 'cf enable-service-access "$SERVICE_NAME" -o org2'
+    end
+  end
 end
-
