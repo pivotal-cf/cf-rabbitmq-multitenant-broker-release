1245d2e66db9877ada0e94102779ce87660790a9
Aitor Perez Cedres <acedres@pivotal.io>
2020-08-18 16:28:58 +0000

    Add variables required to deploy in CI
    [#174337902]
    
    Co-authored-by: Michal Kuratczyk <mkuratczyk@pivotal.io>
    Co-authored-by: Aitor Perez Cedres <acedres@pivotal.io>

--- manifests/ci/create-manifest.sh
+++ manifests/ci/create-manifest.sh
@@ -2,13 +2,18 @@
 
 set -euxo pipefail
 
+ENVIRONMENT="$(cat environment-lock/name)"
+
 bosh interpolate --var deployment-name=cf-rabbitmq-multitenant-broker-release-ci \
 	--var-errs --ops-file=git-bosh-release/manifests/add-cf-rabbitmq.yml \
 	--ops-file=git-bosh-release/manifests/change-vcap-password.yml \
 	--ops-file=git-bosh-release/manifests/add-go-syslogd.yml \
 	--ops-file=git-bosh-release/manifests/add-syslog.yml \
+	--ops-file=git-bosh-release/manifests/add-bosh-dns-aliases.yml \
 	--ops-file=cf-rabbitmq-pipelines/manifests/ops-files/add-embedded-tests.yml \
 	--vars-file=cf-rabbitmq-pipelines/manifests/vars-files/cf-rabbitmq-vars.yml \
 	--vars-file=cf-rabbitmq-pipelines/manifests/vars-files/cf-rabbitmq-multitenant-broker-vars.yml \
 	--vars-file=cf-rabbitmq-pipelines/manifests/vars-files/smith-cf-deployment-vars.yml \
+	--var cf-admin-password="((/bosh-${ENVIRONMENT}/cf/cf_admin_password))" \
+	--var system-domain="${ENVIRONMENT}.cf-app.com" \
 	--var-file stemcell-version=./stemcell-resource/version git-bosh-release/manifests/cf-rabbitmq-broker-template.yml > manifest/manifest.yml
