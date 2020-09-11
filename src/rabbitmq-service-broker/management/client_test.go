05d99bc66c3424b05dade7eb7577949f93b64b1e
Connor Rogers <crogers@pivotal.io>
2020-09-11 09:23:46 +0000

    Refactor rabbithole client into seperate package & add unit tests
    [#165881608]

---
+++ src/rabbitmq-service-broker/management/client_test.go
@@ -0,0 +1,74 @@
+package management_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"rabbitmq-service-broker/config"
+	. "rabbitmq-service-broker/management"
+)
+
+var _ = Describe("NewClient", func() {
+	var brokerConfig config.Config
+	When("Management over TLS is not configured", func() {
+		BeforeEach(func() {
+			brokerConfig = config.Config{RabbitMQ: config.RabbitMQ{
+				Hosts: []string{"127.0.0.1", "127.0.0.2"},
+				Administrator: config.AdminCredentials{
+					Username: "alexandra",
+					Password: "ardnaxela",
+				},
+			}}
+		})
+		It("produces a RabbitMQ management client pointing at the non-TLS endpoint", func() {
+			client, err := NewClient(brokerConfig)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(client.Endpoint).To(Equal("http://127.0.0.1:15672"))
+			Expect(client.Username).To(Equal("alexandra"))
+			Expect(client.Password).To(Equal("ardnaxela"))
+		})
+	})
+
+	When("Management over TLS is configured", func() {
+		BeforeEach(func() {
+			brokerConfig = config.Config{RabbitMQ: config.RabbitMQ{
+				Hosts: []string{"127.0.0.1", "127.0.0.2"},
+				Administrator: config.AdminCredentials{
+					Username: "alexandra",
+					Password: "ardnaxela",
+				},
+				ManagementTLS: config.ManagementTLS{
+					Enabled: true,
+				},
+			}}
+		})
+		It("produces a RabbitMQ management client pointing at the non-TLS endpoint", func() {
+			client, err := NewClient(brokerConfig)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(client.Endpoint).To(Equal("https://127.0.0.1:15671"))
+			Expect(client.Username).To(Equal("alexandra"))
+			Expect(client.Password).To(Equal("ardnaxela"))
+		})
+	})
+
+	When("The broker is configured with an external load balancer", func() {
+		BeforeEach(func() {
+			brokerConfig = config.Config{RabbitMQ: config.RabbitMQ{
+				Hosts:   []string{"127.0.0.1", "127.0.0.2"},
+				DNSHost: "abc.123.com",
+				Administrator: config.AdminCredentials{
+					Username: "alexandra",
+					Password: "ardnaxela",
+				},
+			}}
+		})
+		It("uses the URL of the load balancer in the client endpoint", func() {
+			client, err := NewClient(brokerConfig)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(client.Endpoint).To(Equal("http://abc.123.com:15672"))
+			Expect(client.Username).To(Equal("alexandra"))
+			Expect(client.Password).To(Equal("ardnaxela"))
+		})
+	})
+
+})
