abf69b4ed6dd77d7444660d9a709d681c0a81e01
Apurva Saraiya <apurva.saraiya@broadcom.com>
2025-02-19 19:36:11 +0530

    TNZ-20547: update manifests with the tas-lite values

--- manifests/ci/create-manifest.sh
+++ manifests/ci/create-manifest.sh
@@ -2,9 +2,16 @@
 
 set -euxo pipefail
 
-ENVIRONMENT="$(jq -r '.name' environment-lock/metadata)"
-API_URL="$(jq -r '.cf.api_url' environment-lock/metadata)"
+ENVIRONMENT_LOCK_FILE="${ENVIRONMENT_LOCK_FILE:='environment-lock/metadata'}"
+ENVIRONMENT="$(jq -r '.name' "$ENVIRONMENT_LOCK_FILE")"
+API_URL="$(jq -r '.cf.api_url' "$ENVIRONMENT_LOCK_FILE")"
 DOMAIN=${API_URL//api./}
+
+set +x
+  eval "$(smith -l "$ENVIRONMENT_LOCK_FILE" bosh)"
+  eval "$(smith -l "$ENVIRONMENT_LOCK_FILE" om)"
+  cf_guid="$(om -k curl -s -p /api/v0/staged/products | jq -r '.[] | select(.type == "cf") | .guid')"
+set -x
 
 bosh interpolate \
 	--var deployment-name=cf-rabbitmq-multitenant-broker-release-ci \
@@ -14,13 +21,13 @@ bosh interpolate \
 	--ops-file=git-bosh-release/manifests/change-vcap-password.yml \
 	--ops-file=git-bosh-release/manifests/add-go-syslogd.yml \
 	--ops-file=git-bosh-release/manifests/add-syslog.yml \
-	--ops-file=git-bosh-release/manifests/add-bosh-dns-aliases.yml \
 	--ops-file=cf-rabbitmq-pipelines/manifests/ops-files/add-embedded-tests.yml \
 	--vars-file=cf-rabbitmq-pipelines/manifests/vars-files/cf-rabbitmq-vars.yml \
 	--vars-file=cf-rabbitmq-pipelines/manifests/vars-files/cf-rabbitmq-multitenant-broker-vars.yml \
 	--vars-file=cf-rabbitmq-pipelines/manifests/vars-files/smith-cf-deployment-vars.yml \
-	--var cf-admin-password="((/bosh-${ENVIRONMENT}/cf/cf_admin_password))" \
-	--var nats-client-cert="((/bosh-${ENVIRONMENT}/cf/nats_client_cert.certificate))" \
-	--var nats-client-key="((/bosh-${ENVIRONMENT}/cf/nats_client_cert.private_key))" \
+	--var cf-admin-password="$(om -k curl -s -p "/api/v0/deployed/products/$cf_guid/credentials/.uaa.admin_credentials" | jq -r .credential.value.password)" \
+	--var nats-client-cert="((/opsmgr/${BOSH_DEPLOYMENT}/nats_client_cert.cert_pem))" \
+	--var nats-client-key="((/opsmgr/${BOSH_DEPLOYMENT}/nats_client_cert.private_key_pem))" \
 	--var system-domain="$DOMAIN" \
+	--var cf-deployment-name="${BOSH_DEPLOYMENT}" \
 	--var-file stemcell-version=./stemcell-resource/version git-bosh-release/manifests/cf-rabbitmq-broker-template.yml > manifest/manifest.yml
